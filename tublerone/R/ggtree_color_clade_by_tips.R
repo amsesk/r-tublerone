6cc56fd6f8e3bb1960c827f3d23efa1205152dbb
amsesk <amsesk@oregonstate.edu>
2022-01-05 17:10:48 -0800

    old updates

--- tublerone/R/ggtree_color_clade_by_tips.R
+++ tublerone/R/ggtree_color_clade_by_tips.R
@@ -1,4 +1,4 @@
-ggtree_color_clade_by_tips = function(tree, ggtree, df, clade_column, tip_column, strict = T) {
+ggtree_color_clade_by_tips = function(tree, ggtree, df, clade_column, tip_column, strict = T, return_node_numbers = F) {
   unique_clades = df %>%
     select(!!sym(clade_column)) %>%
     unique %>%
@@ -11,6 +11,7 @@ ggtree_color_clade_by_tips = function(tree, ggtree, df, clade_column, tip_column
   ggtree$data = ggtree$data %>%
     mutate(edgecol = NA)
 
+  node_numbers = list()
   for (u in unique_clades) {
     clade_tips = df %>%
       filter(!!sym(clade_column) == u) %>%
@@ -19,6 +20,8 @@ ggtree_color_clade_by_tips = function(tree, ggtree, df, clade_column, tip_column
     #print(clade_tips)
     clade_node = tublerone::get_ancestral_node_of_tips(tree, clade_tips, strict = strict)
 
+    node_numbers[[u]] = clade_node
+
     clade_node_desc = phytools::getDescendants(tree, clade_node)
 
     ggtree_data_indices = which(ggtree$data$node %in% clade_node_desc)
@@ -26,6 +29,10 @@ ggtree_color_clade_by_tips = function(tree, ggtree, df, clade_column, tip_column
     print(ggtree_data_indices)
 
     ggtree$data$edgecol[ggtree_data_indices] = u
+  }
+
+  if (return_node_numbers) {
+    return(node_numbers)
   }
 
   return(ggtree$data)
