1c0e6eba2cc038908007671957175dc34534b44f
Ubuntu <ubuntu@ip-10-128-254-37.ec2.internal>
2024-02-06 23:10:27 +0000

    move pkg to root

--- tublerone/R/tree_y.R
+++
@@ -1,9 +0,0 @@
-### Credit to https://thackl.github.io/ggtree-composite-plots, thackl on GitHub
-### With minor adaptation
-
-tree_y <-  function(ggtree, data){
-  if(!inherits(ggtree, "ggtree"))
-    stop("not a ggtree object")
-  left_join(select(data, label), select(ggtree$data, label, y)) %>%
-    pull(y)
-}
