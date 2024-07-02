eee297fbb8336fa8741c3068d705c9882de120fe
amsesk <amsesk@upenn.edu>
2024-07-02 18:50:14 +0000

    initial

---
+++ R/mtx_to_seurat.R
@@ -0,0 +1,32 @@
+library(Matrix)
+library(Seurat)
+
+mtx_to_seurat = function(
+                        mtx,
+                        obs,
+                        var,
+                        barcode_column = "barcode",
+                        feature_column = "feature"
+                        ) {
+
+  counts = t(Matrix::readMM(mtx))
+  cell.metadata = read.table(
+                         obs,
+                         sep = ",",
+                         header = T
+  )
+  feature.metadata = read.table(
+                         var,
+                         sep = ",",
+                         header = T
+                         )
+  colnames(counts) = cell.metadata[,barcode_column]
+  rownames(counts) = feature.metadata[,feature_column]
+  
+  rownames(cell.metadata) = cell.metadata[,barcode_column]
+
+  ref_so = Seurat::CreateSeuratObject(counts = counts, meta.data = cell.metadata)
+
+  ref_so
+
+}
