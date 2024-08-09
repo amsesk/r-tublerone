573048e34c18b72f8d678382a5a12007ec5f1855
amsesk <amsesk@umich.edu>
2024-08-09 18:57:39 +0000

    my function was slow, this one is better

--- R/granges.R
+++ R/granges.R
@@ -1,31 +1,12 @@
 library(GenomicRanges)
 
-expandGRange = function(gr, upstream = 2000, downstream = 1000) {
-  if (length(gr) > 1) {
-    message("Length of GRanges object passed to expandGRange must be 1.")
-    stop()
-  } else {
-    chrom = as.character(chrom(gr))
-    chrom_start = 0
-    chrom_end = unname(seqlengths(seqinfo(gr))[chrom])
-    if (as.character(strand(gr)) == "-") {
-      new_start = start(gr) - downstream
-      new_end = end(gr) + upstream
-    } else {
-      new_start = start(gr) - upstream
-      new_end = end(gr) + downstream
-    }
-    if (new_start < chrom_start) {
-      new_start = chrom_start
-    }
-    if (new_end > chrom_end) {
-      new_end = chrom_end
-    }
-    start(gr) = new_start
-    end(gr) = new_end
-    gr
-  }
-}
-grapply = function(granges, func) {
-  unlist(GRangesList(lapply(seq_along(granges), function(i) func(granges[i]))))
+extend_granges <- function(x, upstream=0, downstream=0)     
+{
+    if (any(strand(x) == "*"))
+        warning("'*' ranges were treated as '+'")
+    on_plus <- strand(x) == "+" | strand(x) == "*"
+    new_start <- start(x) - ifelse(on_plus, upstream, downstream)
+    new_end <- end(x) + ifelse(on_plus, downstream, upstream)
+    ranges(x) <- IRanges(new_start, new_end)
+    trim(x)
 }
