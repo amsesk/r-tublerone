46fadd3f11f73050e12053cf8ffd027a1a27ab39
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2024-10-21 09:53:49 -0700

    Update plots2pane.R

--- R/plots2pane.R
+++ R/plots2pane.R
@@ -17,7 +17,7 @@
 #' plots[[2]] = ggplot(dataset, aes(x=x, y=y)) + geom_line()
 #' plots2pane(plots, 1, 2)
 #'
-#' @importFrom ggplot2 ggplot ggplotGrob
+#' @importFrom ggplot2 ggplot ggplotGrob theme geom_blank
 #' @importFrom grid grid.newpage grid.draw
 #' @export
 
